a35cfdb7cf69f40aa7d4df8ff28168a06df174bb
madarax <madarax@madarax.local>
2015-11-20 09:08:20 +0800

    11.20

---
+++ WeCommunity/Class/Util/Categories/UINavigationController+BackItem.h
@@ -0,0 +1,14 @@
+//
+//  UINavigationController+BackItem.h
+//  WeCommunity
+//
+//  Created by madarax on 15/11/19.
+//  Copyright © 2015年 Harry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UINavigationController+BackItem"
+
+@interface UINavigationController(BackItem)
+
+@end
---
+++ WeCommunity/Class/Util/Categories/UINavigationController+BackItem.m
@@ -0,0 +1,30 @@
+//
+//  UINavigationController+BackItem.m
+//  WeCommunity
+//
+//  Created by madarax on 15/11/19.
+//  Copyright © 2015年 Harry. All rights reserved.
+//
+
+@interface UINavigationController ()
+
+@end
+
+@implementation UINavigationController (BackImage)
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
