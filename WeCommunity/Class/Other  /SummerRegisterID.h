844c21ced96a453116c7fa142edf4f5502e6f318
taiduosummer <542596009@qq.com>
2016-06-24 14:02:47 +0800

    2016

--- WeCommunity/Class/Other  /SummerRegisterID.h
+++ WeCommunity/Class/Other  /SummerRegisterID.h
@@ -12,24 +12,24 @@
 
 #endif /* SummerRegisterID_h */
 
-//#define kSummerWxAppID     @"wx8728578ba70796d9"               /**<微信AppID*/
-//#define kSummerWxAppSecret @"d8b5a8bd463de63472970f5452d902b8" /**<微信AppID*/
-//#define MCH_ID   @"1276283701";                       /**<商户号*/
-//#define API_KEY  @"2a19b58eae8e15675a5086207dafd5b2"; /**<密钥*/
+//#define kSummerWxAppID     @""               /**<微信AppID*/
+//#define kSummerWxAppSecret @"" /**<微信AppID*/
+//#define MCH_ID   @"";                       /**<商户号*/
+//#define API_KEY  @""; /**<密钥*/
 //
 //#define SP_URL          @"http://wxpay.weixin.qq.com/pub_v2/app/app_pay.php"
 
 //支付宝
 
-#define kAlipay_Partner     @"2088021074607030"
-#define kAlipay_Seller      @"zhifubao@lixiait.com"
+#define kAlipay_Partner     @""
+#define kAlipay_Seller      @""
 
-#define kAlipay_Private_Key @"MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAN0G9lBa353N3JIzEZ1X+U+eB5DREJfeA1qZHg5bOt/SMX7k0rv2yi0FB3JIDZzZGjPh4zpefbWoHEaSLOmrXx9Fz5rL5rHKH8MPFBVKRnoLGi3We/xO1TAKphL1X4usHV6BgPqzO3qT04uZgJZ7YMWvDN1wwmpdX7RVDn9FJFfJAgMBAAECgYBuUBw9TK2Qr/8nRK/E7PR0ZTX3dDCo61iXLhaPKb8/mEO1xnTa9ThAseLeidGpd72ZKWNkbWamdInwOuKJmUt2fbz52LgXmIyE+PFBdMu0K/TGqhO8T+tnOpqaxPH83n4ra/QTOcN8cb+D3oE+yPHA40A1ttIgS1yDn5/uyksjlQJBAPL7Fo/vJ2zMx6MNsmNyIMvfnLoP/Kw98diNi0YvjFg3zq/VBTFajJnPpKyCH/w7iT4+2qVO4f6uQOGdjkk3AMMCQQDo3r3HGcuGE4kibADxkvsujPvb63eclQribiRmUuXJKYlSblC8QGmShEbdLPFXY9WhaDfx45HIDmZTHO1t1fyDAkBiVM7KrQQNecoI6JAawqMXHoV0nueoZOEsvAty+ecYBG22psSVNonT7T49OePr1cQy1ojxgTzgLnC/XV2pr4K1AkBuJNyyjZrcQeU9KMSRfqnb7Mc7z/X1U/GgA+SnmVIigcj3+kGpqPXGXVZ/SsCkSw5Cl97ORIsxis4PRcFvkCUtAkEA8m04Qx33miidL6tZZqnpDjdfjtqCs3T9OoDpxLxhYCbPvt9z2s4L2+wSnqi7lmD/EKvqiDiihOf+wAsvO+V/bA=="
+#define kAlipay_Private_Key @""
 
 
-#define kAlipay_Public_Key @"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB"
+#define kAlipay_Public_Key @""
 
-#define kAlipay_ReUrl @"http://www.xiaoquhezi.com/pay/zfb/notify/pay" //回调地址
+#define kAlipay_ReUrl @"" //回调地址
 
 
 
